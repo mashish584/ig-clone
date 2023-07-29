56f803db885f3c34cd6f6cc7fce276066625e99d
Ashish Mehra <sdashish.am@gmail.com>
2023-07-30 01:24:55 +0530

    add: Policy for dynamoDB action access in lambda

--- amplify/backend/function/rninstagramPostConfirmation/src/custom.js
+++ amplify/backend/function/rninstagramPostConfirmation/src/custom.js
@@ -1,9 +1,76 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const env = process.env.ENV;
+const appSyncID = process.env.API_RNINSTAGRAM_GRAPHQLAPIIDOUTPUT;
+const TableName = `User-${appSyncID}-${env}`;
+
+const isUserExist = async id => {
+  try {
+    const params = {
+      TableName,
+      Key: id,
+    };
+
+    const response = await documentClient.get(params).promise();
+    return !!response?.Item;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+const saveUser = async user => {
+  try {
+    const date = new Date();
+    const dateString = date.toISOString();
+    const timestamp = date.getTime();
+
+    const Item = {
+      ...user,
+      __typename: 'User',
+      createdAt: dateString,
+      updatedAt: dateString,
+      _lastChangedAt: timestamp,
+      _version: 1,
+    };
+
+    const params = {
+      TableName,
+      Item,
+    };
+
+    await documentClient.put(params).promise();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 exports.handler = async (event, context) => {
-  // insert code to be executed by your lambda trigger
-  console.log('Hi Lambda');
-  console.log(event);
+  if (!event?.request?.userAttributes) {
+    console.log('No user data available');
+    return;
+  }
+
+  const userAttributes = event.request.userAttributes;
+
+  const newUser = {
+    id: userAttributes.sub,
+    name: userAttributes.name,
+    email: userAttributes.email,
+  };
+
+  // check if user already exist
+  if (!isUserExist(newUser.id)) {
+    // if not, save the user to database
+    await saveUser(newUser);
+    console.log(`User ${newUser.id} already exist`);
+  } else {
+    console.log(`User ${newUser.id} already exist`);
+  }
+
   return event;
 };
--- amplify/backend/function/rninstagramPostConfirmation/src/index.js
+++ amplify/backend/function/rninstagramPostConfirmation/src/index.js
@@ -1,4 +1,10 @@
-/**
+/* Amplify Params - DO NOT EDIT
+	API_RNINSTAGRAM_GRAPHQLAPIENDPOINTOUTPUT
+	API_RNINSTAGRAM_GRAPHQLAPIIDOUTPUT
+	API_RNINSTAGRAM_GRAPHQLAPIKEYOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT *//**
  * @fileoverview
  *
  * This CloudFormation Trigger creates a handler which awaits the other handlers
